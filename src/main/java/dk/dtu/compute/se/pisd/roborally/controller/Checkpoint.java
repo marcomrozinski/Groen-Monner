395190a174db4015206819e070a4a70722d2d5ac
marcomrozinski <s245960@dtu.dk>
2025-03-07 10:53:57 +0100

    Lavet så checkpoints skal haves i kronologisk rækkefølge

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
@@ -13,6 +13,7 @@ public class Checkpoint extends FieldAction {
         this.checkpointID = checkpointID;
     }
 
+
     public int getCheckpoint() { //Gemmer checkpoint ID for det respektive checkpoint
         return checkpointID;
 
@@ -22,10 +23,12 @@ public class Checkpoint extends FieldAction {
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
         Player player = space.getPlayer();
         if (player != null && player == gameController.board.getCurrentPlayer()) {
-            if (!player.getReachedCheckpoint().contains(checkpointID)) {
-                player.getReachedCheckpoint().add(checkpointID);
+            if (checkpointID == 1 || player.getReachedCheckpoint().contains(checkpointID - 1)) {
+                if (!player.getReachedCheckpoint().contains(checkpointID)) {
+                    player.getReachedCheckpoint().add(checkpointID);
+                    return true;
+                }
             }
-            return true;
             // Checker hvis en spiller ikke har fået et givet checkpointID, og dernæst tilføjer det til dered ReachedCheckpoint liste
         }
         return false;
